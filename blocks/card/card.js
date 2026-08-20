f16d27e87c8e6a54cbb0b93c649e2a925c6c7db4
jenarthan52 <jenarthan52@gmail.com>
2026-08-20 23:00:52 +0530

    lint error

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -1,17 +1,14 @@
-export default function decorate(block)
-{
-    const cards = [...block.children];
-    cards.forEach((card) => { 
-        card.classList.add('custom-card');
-        const cardnumber = card.querySelector('h2');
-        cardnumber?.classList.add('custom-card__number');
-        const image = card.querySelector('p:has(img)');
-        image?.classList.add('custom-card__image');
-        const description = card.querySelector('p:not(:has(img))');
-        description?.classList.add('custom-card__description');
-        const title = card.querySelector('h3');
-        title?.classList.add('custom-card__title');
-        
-    });   
-
+export default function decorate(block) {
+  const cards = [...block.children];
+  cards.forEach((card) => {
+    card.classList.add('custom-card');
+    const cardnumber = card.querySelector('h2');
+    cardnumber?.classList.add('custom-card__number');
+    const image = card.querySelector('p:has(img)');
+    image?.classList.add('custom-card__image');
+    const description = card.querySelector('p:not(:has(img))');
+    description?.classList.add('custom-card__description');
+    const title = card.querySelector('h3');
+    title?.classList.add('custom-card__title');
+  });
 }
