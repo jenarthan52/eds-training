1aca974b26545fa250625e042aeec21213314987
jenarthan52 <jenarthan52@gmail.com>
2026-08-24 20:49:45 +0530

    faq-tabs

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -3,12 +3,12 @@ export default function decorate(block) {
   cards.forEach((card) => {
     card.classList.add('custom-card');
     const cardnumber = card.querySelector('h2');
-    cardnumber?.classList.add('custom-card__number');
+    cardnumber?.classList.add('custom-card-number');
     const image = card.querySelector('p:has(img)');
-    image?.classList.add('custom-card__image');
+    image?.classList.add('custom-card-image');
     const description = card.querySelector('p:not(:has(img))');
-    description?.classList.add('custom-card__description');
+    description?.classList.add('custom-card-description');
     const title = card.querySelector('h3');
-    title?.classList.add('custom-card__title');
+    title?.classList.add('custom-card-title');
   });
 }
